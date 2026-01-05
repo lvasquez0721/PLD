9ab4d19213d87bce6962b1eebdad596cb6cb6ede
Maximiliano Perea Orozco <mpereaorozco@gmail.com>
2026-01-05 10:54:53 -0600

    se areglo catCategoriaPersonasBloqueadas

--- app/Models/CatCategoriaPersonasBloqueadas.php
+++ app/Models/CatCategoriaPersonasBloqueadas.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class CatCategoriaPersonasBloqueadas extends Model
 {
     // Nombre de la tabla
-    protected $table = 'cat_categoria_personas_bloqueadas';
+    protected $table = 'catCategoriaPersonasBloqueadas';
 
     // Nombre de la clave primaria
     protected $primaryKey = 'ID';
