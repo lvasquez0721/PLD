35330e7b0bfbc0b3b24714d8d620c1c5d4c0cfb8
Maximiliano Perea Orozco <mpereaorozco@gmail.com>
2026-01-23 00:34:05 -0600

    updates alertas

---
+++ app/Models/TbPagosAlertas.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class TbPagosAlertas extends Model
+{
+    protected $table = 'tbPagosAlertas';
+    protected $primaryKey = 'IDPagoAlerta';
+    public $incrementing = true; // El PK es autoincremental según la migración
+    protected $keyType = 'int';
+    public $timestamps = true;
+
+    protected $fillable = [
+        'IDOperacionPago',
+        'IDRegistroAlerta',
+        'InstrumentoMonetario',
+    ];
+
+
+}
