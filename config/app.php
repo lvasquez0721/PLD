ada71d01d28311f7c346801259a9cd8df8877e20
mperea <maximilianoismaelpereaorozco@gmail.com~>
2026-09-02 10:22:47 -0600

    updates

--- config/app.php
+++ config/app.php
@@ -60,12 +60,12 @@ return [
     |--------------------------------------------------------------------------
     |
     | Here you may specify the default timezone for your application, which
-    | will be used by the PHP date and date-time functions. The timezone
-    | is set to "UTC" by default as it is suitable for most use cases.
+    | will be used by the PHP date and date-time functions. Por defecto se
+    | usa la zona horaria de la Ciudad de México (America/Mexico_City).
     |
     */
 
-    'timezone' => 'UTC',
+    'timezone' => env('APP_TIMEZONE', 'America/Mexico_City'),
 
     /*
     |--------------------------------------------------------------------------
